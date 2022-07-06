569b9d32c9577495bd0a88a89685a2399c85f86c
Ben Fulcher <ben.d.fulcher@gmail.com>
2022-07-06 10:19:18 +1000

    Tweaking

--- setup.py
+++ setup.py
@@ -8,16 +8,12 @@ sourceFileList = [os.path.join(sourceDir, file) for file in os.listdir(sourceDir
     # and not (file == "sampen.c" or file == "run_features.c")]
 
 # The c++ extension module:
-extension_mod = Extension(name="catch22_C",
+extension_mod = Extension(name = "catch22_C",
     sources = sourceFileList,
-	include_dirs = [sourceDir]) # Header files are here
-
-# [os.path.join("C", "catch22_wrap.c")],
-# setup(name = "catch22", ext_modules=[extension_mod])
+    include_dirs = [sourceDir])  # Header files are here
 
 setup(
-    name = "pycatch22",
-    include_dirs = [sourceDir],
+    include_dirs = [os.path.join("..", "C"), os.path.join("src", "C")],
     ext_modules = [extension_mod],
-    packages = find_packages(exclude=["test"])
+    include_package_data = True
 )
