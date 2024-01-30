a407e75ff2790195e6783298f220e9c250c8f5d9
Christopher Morton <cjmorton7@icloud.com>
2024-01-30 15:34:37 +1100

    Ensuring installation works for all systems

--- setup.py
+++ setup.py
@@ -8,11 +8,19 @@ sourceFileList = [os.path.join(sourceDir, file) for file in os.listdir(sourceDir
     ".c") and not 'main' in file]
     # and not (file == "sampen.c" or file == "run_features.c")]
 
+cflags = sysconfig.get_config_var('CFLAGS')
+if cflags is not None:
+    extra_compile_args = cflags.split()
+else: # Windows system
+    extra_compile_args = []
+
+extra_compile_args += ["-std=c99"]
+
 # The c++ extension module:
 extension_mod = Extension(name = "catch22_C",
     sources = sourceFileList,
     include_dirs = [sourceDir],
-    extra_compile_args = ["-std=c99"])  # Header files are here
+    extra_compile_args = extra_compile_args)  # Header files are here
 
 setup(
     packages = find_packages(where = "src",
